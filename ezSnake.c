a6ad62fdf6844cec3cf2dcf930a291cc697a81a0
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-09-20 23:07:13 +0530

    trying something different

---
+++ ezSnake.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int snake = 1;
+    printf("Snake is at position : %d\n", snake);
+    snake++;
+    printf("Snake moved to position: %d\n",snake);
+    return 0;
+}
